5f5fb74696228010347574230355c13c287cb63a
Tayseer <taedouni@gmail.com>
2017-04-23 13:32:16 -0700

    Updated Test Data

---
+++ main.py
@@ -0,0 +1,10 @@
+# Number of Spam emails 1397
+# Number of Ham emails 5051
+#Total emails = 6448
+
+#Prob(Spam) = 0.21665632754
+#Prob(~Spam) = 1- P(spam)= 0.78334367246
+
+
+if __name__ == '__main__':
+    main()
