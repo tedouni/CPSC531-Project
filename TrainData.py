cf665ebcd6f823e7065fd6057c2ae313497a3ebe
Tayseer <taedouni@gmail.com>
2017-05-01 13:41:05 -0700

    Sanket code added without modification

--- TrainData.py
+++ TrainData.py
@@ -7,7 +7,14 @@ def analyzeEmail(bodyOfEmail, isSpam, totalSpamWord, totalHamWord):
         #TODO:Check if word exist in database
         #if so increase  for spam frequency
         #else create entry for data base and set spam frequency to 1 and HAM =0
-        totalSpamWord += 1
+         totalSpamWord += 1
+         for word in bodyOfEmail:
+             if db.checkWords(word)>0:
+                #update frequency
+                db.updateFrequencies(word, False)
+             else:
+                 db.insertWords(word,1,0)
+
 
 
     else:   #Not Spam
@@ -15,7 +22,12 @@ def analyzeEmail(bodyOfEmail, isSpam, totalSpamWord, totalHamWord):
     #if so increase for ham frequency
     #Else create entry for database and set HAM (not spam) frequency to 1 and spam = 0
         totalHamWord += 1
-
+        for word in bodyOfEmail:
+            if db.checkWords(word) > 0:
+                #update frequency
+                db.updateFrequencies(word, True)
+            else:
+                db.insertWords(word, 0, 1)
 
 def train(totalSpamWord,totalHamWord,totalEmail,numberOfSpam,numberOfHam,pIsSpam,pIsHam):
 
@@ -32,9 +44,10 @@ def train(totalSpamWord,totalHamWord,totalEmail,numberOfSpam,numberOfHam,pIsSpam
     #SPAM
     for fileName in os.listdir(pathSpam):
         wordList = parseEmail(pathSpam + fileName)
+        print wordList
         numberOfSpam += 1
         totalEmail += 1
-        analyzeEmail(wordList,True,totalSpamWord,totalHamWord)
+        #analyzeEmail(wordList,True,totalSpamWord,totalHamWord)
 
 
     #HAM
@@ -42,7 +55,7 @@ def train(totalSpamWord,totalHamWord,totalEmail,numberOfSpam,numberOfHam,pIsSpam
         wordList= parseEmail(pathHam + fileName)
         numberOfHam += 1
         totalEmail += 1
-        analyzeEmail(wordList,False,totalSpamWord,totalHamWord)
+        #analyzeEmail(wordList,False,totalSpamWord,totalHamWord)
 
 
     pIsHam = float(numberOfHam)/float(totalEmail)
--- createDatabase.py
+++ createDatabase.py
@@ -1,18 +1,33 @@
 import sqlite3
-conn = sqlite3.connect('example.db')
 
+conn = sqlite3.connect('spamfilter.db')
 c = conn.cursor()
+def createTables():
 
-# Create table
-c.execute('''CREATE TABLE stocks
-             (date text, trans text, symbol text, qty real, price real)''')
+    c.execute('''CREATE TABLE wordfilter
+                 (word text UNIQUE, spamfrequency real, hamfrequency real, spamprobability real, hamprobability real)''')  # Create table
 
+def insertWords(word,spamfrequency,hamfrequency):
+    try:
+      c.execute("INSERT INTO wordfilter VALUES (?,?,?,?,?)",[word,spamfrequency,hamfrequency,0,0])
+    except:
+      word
+        # c.execute("INSERT INTO wordfilter VALUE (?)", word)
 # Insert a row of data
-c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
-
+def checkWords(word):
+    res=c.execute("select word from wordfilter where word ='{}'".format(word))
+    return  res.rowcount
 # Save (commit) the changes
-conn.commit()
+
+def updateFrequencies(word,isHam):
+    if isHam:
+        c.execute("update wordfilter set hamfrequency=hamfrequency+{} where word='{}'".format(1,word))
+    else:
+        c.execute("update wordfilter set spamfrequency=spamfrequency+{} where word='{}'".format(1, word))
 
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
-conn.close()
+
+def endDbOperations():
+    conn.commit()
+    conn.close()
